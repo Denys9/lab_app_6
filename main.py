014066aeeaa0c66290a4b47b855e17b754bf4098
lozin <lozindenis6@gmail.com>
2022-10-29 13:07:55 +0300

    Create work "№2"

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-def
+
+def str():
     try:
+        text = input('str - ')
+        counter = 0
+        counter2 = 0
+        for i in text:
+            if i.isalpha():
+                counter+=1
+            if i.isnumeric():
+                counter2+=1
+        print(f'Num of letters -{counter}\nNum of digits - {counter2}')
 
 
 
+    except Exception as ex:
+       print(f'Eror information: {ex}')
 
-except Exception as ex:
-    print(f'Eror information: {ex}')
+
+str()
